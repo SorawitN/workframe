6aa3532de6e1f735a3f246a6eb107db7e5233916
Sorawit <sorawit.nu@ku.th>
2020-10-28 17:19:03 +0700

    create portfolio_developer page

---
+++ src/pages/portfolio_developer.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+function Portfolio_developer() {
+  //State
+  
+
+
+  //Function
+  
+
+  return (
+    <div>
+      <h1>This is developer portfolio page</h1>
+    </div>
+  );
+}
+
+export default Portfolio_developer;
